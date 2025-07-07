395316a420bdf4036a30ef33bbf9bed9fda5b935
EurekaShadow <3378442913@qq.com>
2025-07-07 23:53:53 +0800

    1、补充添加CNAME文件的正确做法； 2、

--- src/components/ForMDX.js
+++ src/components/ForMDX.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import 'react-photo-view/dist/react-photo-view.css';
-import BStyles from '@site/src/css/MyCss.module.css';
+import Styles from '@site/src/css/MyCss.module.css';
 
 import Lazyimg from 'react-lazyimg-component';// 懒加载插件
 
@@ -47,7 +47,7 @@ export function Highlight({children, bgColor = MyColor.yellow, fontColor = MyCol
 
 export function Keyword({children}) {
   return (
-    <span className={BStyles.InlineHighlight}
+    <span className={Styles.InlineHighlight}
       style={{
         padding: '0.2rem',        // 内部留白（已有）
         margin: '0.4rem 0.4rem',       // 左右各 0.4rem 空隙，上下为 0
@@ -61,13 +61,13 @@ export function Keyword({children}) {
 /*************************************************************************************
  * 功能：为一级标题添加装饰性样式（如下方渐变横线）
  * 说明：
- *   - 使用 <span> 包裹标题内容，并添加 className={BStyles.B1}
+ *   - 使用 <span> 包裹标题内容，并添加 className={Styles.B1}
  *   - 实际装饰效果由 CSS 控制（如渐变线条 + hover 拉伸动画）
  *   - 命名从 H1 改为 B1 是为了避免与 HTML 标签冲突
  *************************************************************************************/
 export function B1({children}) {
   return (
-    <span className={BStyles.B1}>
+    <span className={Styles.B1}>
       {children}
     </span>
   )
@@ -75,13 +75,13 @@ export function B1({children}) {
 /*************************************************************************************
  * 功能：为二级标题添加装饰性样式（如下方渐变横线）
  * 说明：
- *   - 使用 <span> 包裹标题内容，并添加 className={BStyles.B2}
+ *   - 使用 <span> 包裹标题内容，并添加 className={Styles.B2}
  *   - 实际装饰效果由 CSS 控制（如渐变线条 + hover 拉伸动画）
  *   - 命名从 H2 改为 B2 是为了避免与 HTML 标签冲突
  *************************************************************************************/
 export function B2({children}) {
   return (
-    <span className={BStyles.B2}>
+    <span className={Styles.B2}>
       {children}
     </span>
   )
@@ -111,14 +111,14 @@ export function B2({children}) {
 *************************************************************************************/
 export function B3({children}) {
   return (
-    <span className={BStyles.B3}>{children}</span>
+    <span className={Styles.B3}>{children}</span>
   );
 }
 
 export function BH3({children}) {
   return (
     <span 
-      className={BStyles.BH3}
+      className={Styles.BH3}
       style={{        
         padding: '0.2rem',
         marginRight: '0.5rem', // 添加右边距
@@ -143,79 +143,14 @@ export function B3W({children}) {
 /* end */
 
 
-/**************************************************************************************
- * 功能：图片点击放大
- * 参数：src：图片地址，alt：图片描述，title：图片标题，isCenter：是否居中显示
- **************************************************************************************/
-export function PhotoZoom(props){
-    return (<p>
-            <PhotoProvider  maskOpacity={0.5} speed={() => 600} >
-                <PhotoView src={props.src} alt={props.alt} title={props.title}>
-                  {props.isCenter? <center><img src={props.src}  style={{cursor: "zoom-in",objectFit: 'cover'}} alt={props.alt} title={props.title}  /></center>:<img src={props.src}  style={{cursor: "zoom-in",objectFit: 'cover'}} alt={props.alt} title={props.title}  />}
-                </PhotoView >
-            </PhotoProvider>
-            </p>)
-}
-
-/* end */
 
 
 
-/**************************************************************************************
- * 功能：把图片框起来
- * 参数：src：图片地址，alt：图片描述，title：图片标题，isCenter：是否居中显示,不写默认为否；
- *       isZoom：是否给图片设置点击放大，不写默认为是；
- **************************************************************************************/
-export function PhotoBox(props){
-  if (props.isMultiple) {
-    return (
-      <div class="card-demo" style={{margin: '10px',padding: '10px',borderRadius: '10px',boxShadow: '0 0 10px rgba(0,0,0,0.1)' , }} className={BStyles.PohtoBoxBorder}>
-          <div class="card-demo" style={{borderStyle: 'solid', borderWidth: '2px',borderColor: '#2ed3af',borderRadius: '10px'}}>
-            {props.children}
-          </div>  
-      </div>  
-    )
-  }
-  else {          
-    return (
-        <div class="card-demo" style={{margin: '10px',padding: '10px',borderRadius: '10px',boxShadow: '0 0 10px rgba(0,0,0,0.1)' , }} className={BStyles.PohtoBoxBorder}>
-            <div class="card-body">
-                <CardImg src={props.src} alt={props.alt} title={props.title} isZoom={props.isZoom} isCenter={props.isCenter}></CardImg>     
-            </div>
-      </div>     
-      )    
-  }        
-}
+const Card = ({ children }) => (
+  <div className={Styles.Card}>{children}</div>
+);
 
-/**************************************************************************************
- * 功能：图片四角稍微有些倒角
- * 参数：src：图片地址，alt：图片描述，title：图片标题，isCenter：是否居中显示,不写默认为否；
- *       isZoom：是否给图片设置点击放大，不写默认为是；
- **************************************************************************************/
-export function CardImg(props){
-    return (
-      <BrowserOnly>
-      {() => {
-        // 这里可以安全使用 document、window 等浏览器 API
-        const element = document.getElementById('my-id');
-        return (
-      <p><div class="card" style={{borderRadius: '8px',boxShadow: '0 0 20px rgba(0,0,0,0.1)' , }} >
-        {/* <div class="item shadow--md"> */}
-            {props.isZoom?                   
-              <Lazyimg className="lazy" src={props.src}  style={{objectFit: 'cover'}} alt={props.alt} title={props.title} />:
-              //<img src={props.src}  style={{objectFit: 'cover'}} alt={props.alt} title={props.title}  />:
-              <PhotoProvider  maskOpacity={0.5} speed={() => 600} >
-                  <PhotoView src={props.src} alt={props.alt} title={props.title}>
-                    {props.isCenter? <center><Lazyimg className="lazy"  width="50%" height="50%" src={props.src} style={{cursor: "zoom-in",objectFit: 'cover'}} alt={props.alt} title={props.title}  /></center>:<Lazyimg className="lazy" src={props.src}  style={{cursor: "zoom-in",objectFit: 'cover'}} alt={props.alt} title={props.title}  />}
-                  </PhotoView >
-              </PhotoProvider>
-            }
-          {/* </div> */}
-      </div></p>
-        )
-         }}
-      </BrowserOnly>
-    )
-}
+// 如果未来需要扩展再启用
+// const CardContent = ({ children }) => <div>{children}</div>;
 
-/* end */
+export { Card };
