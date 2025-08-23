b408057d8d6a6e483ac3f8bec7bd8490bdd1d461
EurekaShadow <3378442913@qq.com>
2025-08-23 18:27:12 +0800

    添加 Vercel 的WebAnalytics

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 
 function Root({ children }) {
   return (
     <>
       {children}
       <SpeedInsights />
+	  <Analytics />
     </>
   );
 }
